018f37226726528cc6e86c82eb0a7077268602e7
Aliyeyyy <aliyeyyy35@gmail.com>
2023-01-30 19:51:59 +0100

    first commit

---
+++ src/test/java/US28/US28_GroceeryTest.java
@@ -0,0 +1,29 @@
+package US28;
+
+import Team06.pages.US28_Groceery.Groceery;
+import Team06.utilities.BaseTestReport;
+import Team06.utilities.Driver;
+import org.testng.annotations.Test;
+
+public class US28_GroceeryTest {
+ @Test
+    public void test(){
+ // extentTest = extentReports.createTest("US028-testCase01");
+
+     //Kullanıcı URL gider
+     Driver.getDriver().get("https://shop-pickbazar-rest.vercel.app/");
+     Groceery gP= new Groceery();
+
+     //Kullanıcı ürün fiyatını görür
+     gP.fiyatDogrula();
+
+     //Kullanıcı indirim oranını görür
+     gP.indirimDogrula();
+
+     //Kullanıcı ürün ilk fiyatını görür
+     gP.eskiFiyatDogrrula();
+
+
+ }
+
+}
